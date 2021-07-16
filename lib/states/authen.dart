5d240cacf4b1e1ac51db4be9e49c00de1f5fd12d
notslim <notslim2475@gmail.com>
2021-07-16 17:28:04 +0700

    update

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -13,6 +13,7 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  bool statusRedEye = true;
   @override
   Widget build(BuildContext context) {
     double size = MediaQuery.of(context).size.width;
@@ -22,29 +23,88 @@ class _AuthenState extends State<Authen> {
           children: [
             buildImage(size),
             buildAppName(),
-           
+            buildUser(size),
+            buildPassword(size),
           ],
         ),
       ),
-    );  
+    );
+  }
+
+  Row buildUser(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: MyConstant().h3Stype(),
+              labelText: 'user :',
+              prefixIcon: Icon(
+                Icons.account_circle_outlined,
+                color: MyConstant.dark,
+              ),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyConstant.dark),
+                  borderRadius: BorderRadius.circular(30)),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyConstant.light),
+                  borderRadius: BorderRadius.circular(30)),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildPassword(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            obscureText: statusRedEye,
+            decoration: InputDecoration(
+              labelStyle: MyConstant().h3Stype(),
+              labelText: 'password :',
+              prefixIcon: Icon(
+                Icons.lock_outline,
+                color: MyConstant.dark,
+              ),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyConstant.dark),
+                  borderRadius: BorderRadius.circular(30)),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyConstant.light),
+                  borderRadius: BorderRadius.circular(30)),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
   Row buildAppName() {
-    return Row(mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ShowTitle(title: MyConstant.appName, textStyle: MyConstant().h2Stype(),
-              ),
-            ],
-          );
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ShowTitle(
+          title: MyConstant.appName,
+          textStyle: MyConstant().h2Stype(),
+        ),
+      ],
+    );
   }
 
   Row buildImage(double size) {
-    return Row(mainAxisAlignment: MainAxisAlignment.center,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Container(
-          width: size * 0.6, 
-          child: ShowImage(path: MyConstant.image4)
-        ),
+        Container(width: size * 0.6, child: ShowImage(path: MyConstant.image4)),
       ],
     );
   }
